32d605fce1291da652a758b54ed735ac5e8326d3
DmitriyAngve <kirirch129@gmail.com>
2023-08-11 12:42:41 +0300

    #

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -5,8 +5,21 @@ import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // fix hydration problem
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+  // fix hydration problem
+
   return (
     <Sheet>
       <SheetTrigger>
