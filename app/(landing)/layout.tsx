11fb1f75f1c5fe1f1756497e7082b178f5c0c246
DmitriyAngve <kirirch129@gmail.com>
2023-08-28 13:02:44 +0700

    #

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,7 +1,6 @@
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="h-full bg-[#111827]">
-      {" "}
+    <main className="h-full bg-[#111827] overflow-auto">
       <div className="mx-auto max-w-screen-xl h-full">{children}</div>
     </main>
   );
